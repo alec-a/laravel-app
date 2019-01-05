8c59766a93afe87e09b52e12b8ed09870dd0f8b2
Alec Aldous <alecda@gmail.com>
2019-01-05 02:03:57 +0000

    Revert "removing the admin section from the sidebar for non admin users"
    This reverts commit a173137304acbcfae2b6f0a7a14ac157c783da5d.

--- resources/views/layouts/userArea.blade.php
+++ resources/views/layouts/userArea.blade.php
@@ -116,7 +116,6 @@ if(isset($dumpTop)){
 							  <li><a href="{{url('/account')}}" class="{{$pageData->activeNav=='account'? 'is-active':''}}">Account</a></li>
 							  <li><a href="{{url('/account/messages')}}" class="{{$pageData->activeNav=='messages'? 'is-active':''}}">Messages</a></li>
 							</ul>
-							@if(auth()->user()->role == 1)
 							<p class="menu-label is-size-6">
 							  Administration
 							</p>
@@ -131,7 +130,6 @@ if(isset($dumpTop)){
 							  <li><a>Applications</a></li>
 							  <li><a>Users</a></li>
 							</ul>
-							@endif
 						</aside>
 					</div>
 				
